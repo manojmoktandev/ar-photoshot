86a07467fcdf912017708e2e78a21a1bb527ae1c
manojmoktan <manojmoktanhunkers@gmail.com>
2020-07-05 16:16:46 +0545

    initial commit

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Digital signage</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="language" content="en">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="public/stylesheets/style.css">
+  <link rel="stylesheet" type="text/css" href="public/stylesheets/slick.css">
+  <link rel="stylesheet" type="text/css" href="public/stylesheets/font-awesome.min.css">
+</head>
+<body>
+  <div class="wrapper">
+    <audio id="myAudio" style="display: none">
+    <source src="public/images/click.mp3" type="audio/ogg">
+    <source src="public/images/click.mp3" type="audio/mpeg">
+    Your browser does not support the audio element.
+    </audio>
+    <div class="three" style="opacity: 0;">
+    </div>
+    <img src="public/images/text.png" class="textimg">
+  <div class="gridBox">
+<!--    <div class="button-group sort-by-button-group">
+      <button class="button is-checked" data-sort-value="original-order">original order</button>
+      <button class="button" data-sort-value="category">category</button>
+    </div> -->
+      <!-- <a href="start" class="start">start</a> -->
+    <div class="grid">
+            <div class="grid-item  grid-item--width2"><img src="public/images/gallery/image8.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image1.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image16.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image18.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image16.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image18.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image16.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2 grid-item--height2"><img src="public/images/gallery/image18.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image5.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2 grid-item--height2"><img src="public/images/gallery/image16.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2 "><img src="public/images/gallery/image7.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image2.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image16.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image18.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image16.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2 grid-item--height2"><img src="public/images/gallery/image17.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2"><img src="public/images/gallery/image10.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image6.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2"><img src="public/images/gallery/image8.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image1.jpg" alt=""></div>
+            <div class="grid-item grid-sizer"><img src="public/images/gallery/image18.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image5.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2"><img src="public/images/gallery/image16.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2 "><img src="public/images/gallery/image7.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image2.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2 grid-item--height2"><img src="public/images/gallery/image17.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2"><img src="public/images/gallery/image10.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image6.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2"><img src="public/images/gallery/image8.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image1.jpg" alt=""></div>
+                        <div class="grid-item grid-item--width2"><img src="public/images/gallery/image10.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image6.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2"><img src="public/images/gallery/image8.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image1.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2 grid-item--height2"><img src="public/images/gallery/image18.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image5.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2 grid-item--height2"><img src="public/images/gallery/image16.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2 "><img src="public/images/gallery/image7.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image2.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2"><img src="public/images/gallery/image17.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2"><img src="public/images/gallery/image10.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image6.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2"><img src="public/images/gallery/image8.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image1.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image5.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image18.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image16.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image18.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image16.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image18.jpg" alt=""></div>
+            <div class="grid-item"><img src="public/images/gallery/image16.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2 "><img src="public/images/gallery/image7.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image2.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2"><img src="public/images/gallery/image17.jpg" alt=""></div>
+            <div class="grid-item grid-item--width2"><img src="public/images/gallery/image10.jpg" alt=""></div>
+            <div class="grid-item grid-item--height2"><img src="public/images/gallery/image6.jpg" alt=""></div>
+        </div>
+  <button class="glow-on-hover bounce-1">Start!</button>
+  </div>
+  <div class="slider" style="display: none;">
+        <div class="container">
+    <div class="select_box">
+    <div class="slider-box">
+      <div class="slider_for">
+        <div><img src="public/images/player/player-1.png"></div>
+        <div><img src="public/images/player/player-2.png"></div>
+        <div><img src="public/images/player/player-3.png"></div>
+        <div><img src="public/images/player/player-4.png"></div>
+        <div><img src="public/images/player/player-5.png"></div>
+        <div><img src="public/images/player/player-1.png"></div>
+      </div>
+      <a class="glow-on-hover bounce-1" href="javaScript:void(0);">Select</a>
+    </div>
+        <div class="pbottom">
+    <div class="slider_nav">
+      <div><img src="public/images/player/thumbs/player-1-thumb.jpg"></div>
+      <div><img src="public/images/player/thumbs/player-2-thumb.jpg"></div>
+      <div><img src="public/images/player/thumbs/player-3-thumb.jpg"></div>
+      <div><img src="public/images/player/thumbs/player-4-thumb.jpg"></div>
+      <div><img src="public/images/player/thumbs/player-5-thumb.jpg"></div>
+      <div><img src="public/images/player/thumbs/player-1-thumb.jpg"></div>
+    </div>
+    </div>
+  </div>
+    <div class="capture_box" style="display: none">
+      <div class="slider-box">
+        <div class="slider_for_next">
+          <div><img src="public/images/player/player-1.png"></div>
+        <div><img src="public/images/player/player-2.png"></div>
+        <div><img src="public/images/player/player-3.png"></div>
+        <div><img src="public/images/player/player-4.png"></div>
+        <div><img src="public/images/player/player-5.png"></div>
+        <div><img src="public/images/player/player-1.png"></div>
+        </div>
+        <button class="glow-on-hover capture bounce-1">Capture</button>
+        <p id="demo" class="countdown" style="display: none;">10</p>
+      </div>
+    <div class="slider_nav_next">
+      <div class="div1">
+        <p id="demo1" class="countdown" style="display: none;">10</p>
+        <img src="public/images/img01-capture.jpg" style="display: none" class="show">
+      </div>
+      <div class="div2">
+        <p id="demo2" class="countdown" style="display: none;">10</p>
+        <img src="public/images/img02-capture.jpg" style="display: none" class="show">
+      </div>
+      <div class="div3">
+        <p id="demo3" class="countdown" style="display: none;">10</p>
+        <img src="public/images/img03-capture.jpg" style="display: none" class="show">
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+    <div class="qrcode" style="display: none;">
+            <!-- <p class="date">2019.12.13</p> -->
+            <p class="thankmsg">THANK YOU</p>
+            <!-- <div class="qrcodebox">
+                <img src="public/images/qrcode.png">
+            </div> -->
+                <p><span class="code"></span></span></p>
+        </div>
+</div>
+  </div>
+</body>
+  <script src = "public/javascripts/socket.io.js"></script>
+  <script src = "config/config.js"></script>
+  <script type="text/javascript">
+      const socketUrl = config['socket_url']+':'+config['client_port'];
+      // const connect = () => {s
+        socket = io(socketUrl, {
+          autoConnect: true,
+        });
+
+        socket.on('connect', () => {
+          console.log('Connected'+config['client_port']);
+         
+        });
+
+        socket.on('disconnect', (reason) => {
+          console.log(`Disconnected: ${reason}`);
+         
+        })
+
+        socket.open();
+      //};
+
+      const disconnect = () => {
+        socket.disconnect();
+      }   
+      socket.on('setSelectedDesktopImage', function(thumb_id) {
+         if(thumb_id) {
+          $('.slider_for').slick('slickGoTo', thumb_id);
+         }
+      }); 
+      socket.on('start', function() {
+         $('.gridBox .glow-on-hover').trigger("click");
+      });
+      socket.on('select', function() {
+         $(".slider-box .glow-on-hover").trigger("click");
+      }); 
+  </script>
+  <script type="text/javascript" src="public/javascripts/jquery.min.js"></script>
+  <script type="text/javascript" src="public/javascripts/custom.js"></script>
+  <script type="text/javascript" src="public/javascripts/slick.min.js"></script>
+  <script type="text/javascript" src="public/javascripts/script.js"></script>
+  <script type="text/javascript" src="public/javascripts/isotope.pkgd.min.js"></script>
+  <script src="https://npmcdn.com/imagesloaded@4/imagesloaded.pkgd.js"></script>
+</html>
